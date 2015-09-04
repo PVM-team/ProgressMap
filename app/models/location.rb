7ee4c41684f5c80c458c055c0418ed2b913b4802
Mika Huttunen <mika.huttunen@1BQQG5E.kumpula.dhcp.hupnet.helsinki.fi>
2015-09-04 15:50:48 +0300

    validations for assignment and location - foreign keys cannot be nil

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,7 @@ class Location < ActiveRecord::Base
 	
 	belongs_to :assignment
 
+	validates :assignment_id, numericality: { only_integer: true }
 	validates :x, numericality: { greater_than_or_equal_to: 0 }
 	validates :y, numericality: { greater_than_or_equal_to: 0 }
 end
