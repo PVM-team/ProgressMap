da365c64ca0aa7723372c83adda1a984217d48e2
Pakarinen Pia P <pppakari@dat-11.cs.helsinki.fi>
2015-09-08 11:30:07 +0300

    map updates: points can be placed by location, curves can be drawn

--- app/assets/javascripts/MapScript.js
+++ app/assets/javascripts/MapScript.js
@@ -1,15 +1,48 @@
 
+window.onload = function() {
+    var locations = document.getElementById("database");
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.fillStyle = "#6B8E23"
-ctx.fillRect(0,0,1000,1000);
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.fillStyle = "#6B8E23";
+    ctx.fillRect(0, 0, 1000, 1000);
 
 
+    var img = new Image(5, 5);
+    img.src = "ball2.png";
 
-var img = new Image(5,5);
-img.src="ball2.png";
+    /*
+     img.onload = function() {
+     ctx.drawImage(img,250,600,70,70);
+     ctx.drawImage(img,500,500,70,70);
+     }
+     */
 
-img.onload = function() {
-    ctx.drawImage(img,250,250,70,70);
+    img.onload = function () {
+        /* for (location in locations){
+         placeImageOnLocation(location.x, location.y, img);
+         }*/
+        /*
+         placeImageOnLocation(250, 600, img);
+         placeImageOnLocation(500, 500, img);
+         placeImageOnLocation(800, 500, img);
+         placeImageOnLocation(700, 300, img);
+         */
+    }
+
+    drawQuadratic(250, 600, 500, 500);
+    drawQuadratic(500, 500, 800, 500);
+    drawQuadratic(800, 500, 700, 300);
+
+
+    function placeImageOnLocation(x, y, img) {
+        ctx.drawImage(img, x, y, 70, 70);
+    }
+
+    function drawQuadratic(x1, y1, x2, y2) {
+        ctx.beginPath();
+        ctx.moveTo((x1 + 40), (y1 + 40));
+        ctx.quadraticCurveTo(((x2 + x1) / 2), x2, (x2 + 40), (y2 + 40));
+        ctx.stroke();
+    }
 }
