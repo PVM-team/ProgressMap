1991dba7fc5940b5515ae4f2ded996a89040fad8
Mika Huttunen <mika.huttunen@Mika-Huttunen.local>
2015-09-11 15:25:27 +0300

    Completed_assignments test for model User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe User do
+describe User, type: :model do
 
   before :each do
     @course = FactoryGirl.create :course
@@ -9,13 +9,35 @@ describe User do
 
   it "creates user for correct course" do
     @course.participants << @user
-
-    expect(@course.participants.length).to be(1)
+    expect(@course.participants.length).to eq(1)
   end
 
-  it "returns empty list if no courses are completed" do
-    completed = @user.completed_assignments(@course)
+  describe "when completed assignments is called" do
 
-    expect(completed.length).to be(0)
+    it "returns empty list if no courses are completed" do
+      completed = @user.completed_assignments(@course)
+      expect(completed.length).to eq(0)
+    end
+
+    it "returns completed assignments when some are done" do
+      course2 = FactoryGirl.create :course
+      assignment1 = FactoryGirl.create :assignment
+      assignment2 = FactoryGirl.create :assignment
+      assignment3 = FactoryGirl.create :assignment
+
+      add_assignment(@course, assignment1)
+      add_assignment(course2, assignment2)
+      add_assignment(course2, assignment3)
+
+      @user.assignments << assignment1
+      @user.assignments << assignment2
+
+      expect(@user.completed_assignments(@course).length).to eq(1)
+      expect(@user.completed_assignments(course2).length).to eq(1)
+    end
   end
+end
+
+def add_assignment(course, assignment)
+  course.assignments << assignment
 end
