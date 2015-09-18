f6876b4c83330cb8117389ecb063bf86b0aa7b04
Andreas Andra <aandra@1BQQFZW.kumpula.dhcp.hupnet.helsinki.fi>
2015-09-18 14:33:52 +0300

    checkIfAssignmentIsDone test

--- spec/javascripts/controllers/MapController_spec.js
+++ spec/javascripts/controllers/MapController_spec.js
@@ -22,7 +22,7 @@ describe('MapController', function () {
         data.course = {"id": 1};
         data.assignments =
             [{"id": 1, "location": {"id": 1, "x": 100, "y": 250}, "doers": [{"id": 2}, {"id": 1}]},
-                {"id": 2, "location": {"id": 2, "x": 330, "y": 180}, "doers": []},
+                {"id": 2, "location": {"id": 2, "x": 330, "y": 180}, "doers": [{"id" : 1}]},
                 {"id": 3, "location": {"id": 3, "x": 500, "y": 130}, "doers": [{"id": 1}]}];
         data.participants = [{"id": 1}, {"id": 2}, {"id": 3}];
         data.current_user = {"id": 2};
@@ -62,6 +62,11 @@ describe('MapController', function () {
 
     it('Init was called on Controller initialize', function () {
         expect(mapDataService.initMap).toHaveBeenCalled();
+    })
+
+    it('if given assigment is done returns true', function () {
+        scope.viewAsStudent(2);
+        expect(scope.checkIfAssignmentIsDone(scope.done_assignments[0].id)).toBe(true);
     })
 
     //ei toimi
