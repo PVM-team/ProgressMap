0f9b2b53ca2418999a09c82eac76a1ac441be650
Pakarinen Pia P <pppakari@dat-13.cs.helsinki.fi>
2015-09-23 15:04:11 +0300

    version without user as part of path/route

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,7 +3,7 @@ var ProgressApp = angular.module('ProgressApp',['ngRoute', 'ngResource'])
 ProgressApp.config(['$routeProvider', function($routeProvider){
    $routeProvider
 
-       .when('/map/:course_id/:user_id', {
+       .when('/map/:course_id', {
            controller: 'MapController',
            templateUrl: 'templates/map.html'
        })
@@ -13,6 +13,6 @@ ProgressApp.config(['$routeProvider', function($routeProvider){
       
        })
        .otherwise({
-           redirectTo: '/map/:course_id?1/:user_id?2'
+           redirectTo: '/map/:course_id?1'
        });
 }]);
