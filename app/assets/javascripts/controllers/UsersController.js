00222537438df4f411bed880c67bfe537bf190b2
Andreas Andra <aandra@localhost.localdomain>
2015-09-24 03:03:19 +0300

    some progress with users..

--- app/assets/javascripts/controllers/UsersController.js
+++ app/assets/javascripts/controllers/UsersController.js
@@ -1,33 +1,21 @@
 ProgressApp.controller('UsersController', function($scope, DataSendService, UserDataService, MapDataService) {
 
     UserDataService.init().then(function(data) {
-        data.courses = [{"id": 1}, {"id": 2}, {"id": 3}]
-
         $scope.courses = data["courses"]
         $scope.users = data["users"]
     })
 
-    $scope.courses = [{"id": 1}, {"id": 2}, {"id": 3}]
-
     $scope.addStudentToCourse = function(course_id, user_id) {
 
-        var userData = {
+        var student = {
             course_id : course_id,
-            user_id : user_id
+            user_id :user_id
         }
 
-        DataSendService.addData('/users', userData).then(function(data) {
-            var newStudent = {
-                id: data.id
-            }
 
             MapDataService.initMap(course_id, user_id).then(function(data) {
                 $scope.participants = data["participants"]
             })
-            $scope.participants.push(newStudent);
-        })
-
-    }
-
-
+            $scope.participants.push(student);
+        }
 })
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,6 +57,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def init
+    @users = User.all
+    @courses = Course.all
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
