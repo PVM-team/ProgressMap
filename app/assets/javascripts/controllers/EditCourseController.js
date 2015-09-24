0334d9455be8e4f9d3a7f4ba4f36435490d85037
Teemu Ijäs <tijas@cs.helsinki.fi>
2015-09-24 17:25:45 +0300

    course edit view update

--- app/assets/javascripts/controllers/EditCourseController.js
+++ app/assets/javascripts/controllers/EditCourseController.js
@@ -1,17 +1,19 @@
 ProgressApp.controller('EditCourseController', function($scope, $routeParams, $location, StateService, httpService) {
     var courses = [];
-    
+
     httpService.getData('courses/all', {}).then(function(data) {
         courses = data['courses'];
         for (var i = 0; i < courses.length; i++) {
             if (courses[i].id == $routeParams.course_id) {
                 $scope.course = courses[i];
                 $scope.originalName = $scope.course.name;
-                console.log(courses[i]);
-                $scope.participants = courses[i]; 
                 break;
             }
         }
 
+        httpService.getData('/map/init.json', {params: {course_id: $routeParams.course_id}}).then(function(data){
+            $scope.assignments = data["assignments"]
+            $scope.participants = data["participants"]
+        });
     });
 });
