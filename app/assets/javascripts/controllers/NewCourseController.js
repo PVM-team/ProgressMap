78d91f254da15245473fdc6f07b6e6f7accad94d
Mika Huttunen <mika.huttunen@Mika-Huttunen.local>
2015-10-05 11:08:34 +0300

    Algorithm for drawing locations modified. Changing directions works now properly.

--- app/assets/javascripts/controllers/NewCourseController.js
+++ app/assets/javascripts/controllers/NewCourseController.js
@@ -77,35 +77,50 @@ ProgressApp.controller('NewCourseController', function ($scope, $location, httpS
             //100 pixeliä lisätään reunoi varten
             CanvasService.initiateCanvas((2 * borderSize + blockSize) * levelAmount + 100, canvasWidth + 100, document.getElementById("mapElements"), "rgba(30, 85, 205, 0.50")
 
+            var direction = "left"; // asetetaan directioniksi 'left', joka vaihtuu heti 'directionOfCurve' funktiossa, koska i % assignmentsPerLevel = 0.
+
             for (var i = 0; i < $scope.assignmentCount; i++) {
-                var location = drawLocationForAssignment(i, assignmentsPerLevel, levelAmount, borderSize, blockSize)
+                var direction = directionOfCurve(direction, i, assignmentsPerLevel);
+                var location = drawLocationForAssignment(i, assignmentsPerLevel, borderSize, blockSize, direction, levelAmount);
 
-                var assignment = {'number': i + 1, 'location': location, 'doers': {}, 'dependencies': {} }
-
-                $scope.assignments.push(assignment)
+                var assignment = {'number': i + 1, 'location': location, 'doers': {}, 'dependencies': {} };
+                $scope.assignments.push(assignment);
             }
-
-            CanvasService.drawSmoothPaths(getLocations())
+            CanvasService.drawSmoothPaths(getLocations());
         }
     }
 
-    function drawLocationForAssignment(i, assignmentsPerLevel, levelAmount, borderSize, blockSize) {
-        var level = Math.ceil((i + 1) / assignmentsPerLevel) - 1
-
-        //50 pixeliä otettaan huomioon reunaa varten
-        if (level % 2 == 0) {
-            var xStart = 50 + borderSize + (i % assignmentsPerLevel * (2 * borderSize + blockSize))
-        } else {
-            var xStart = (50 + (assignmentsPerLevel - 1) * (blockSize + borderSize * 2)) - borderSize - (i % assignmentsPerLevel * (2 * borderSize + blockSize))
-        }
-
-        //50 pixeliä reunaa varten
-        var yStart = 50 + borderSize + level * (2 * borderSize + blockSize)
+    function drawLocationForAssignment(i, assignmentsPerLevel, borderSize, blockSize, direction, levelAmount) {
+        var xStart = defineXStart(i, assignmentsPerLevel, borderSize, blockSize, direction)
+        var yStart = defineYStart(i, assignmentsPerLevel, borderSize, blockSize, levelAmount)
 
         var x = getRandomPosition(xStart, blockSize)
         var y = getRandomPosition(yStart, blockSize)
 
         return {'x': x, 'y': y};
+    }
+
+    function defineXStart(i, assignmentsPerLevel, borderSize, blockSize, direction) {
+        //50 pixeliä otettaan huomioon reunaa varten
+        var border = 50 + 2 * borderSize;
+        var relativeStartingPosition = (i % assignmentsPerLevel) * (2 * borderSize + blockSize);
+
+        if (direction === "right") {
+            return border + relativeStartingPosition;
+        }
+        else if (direction === "left") {
+            return border - relativeStartingPosition + (assignmentsPerLevel - 1) * (2 * borderSize + blockSize);
+        }
+
+        throw "Direction value not defined";
+    }
+
+    function defineYStart(i, assignmentsPerLevel, borderSize, blockSize, levelAmount) {
+        var level = Math.ceil(levelAmount - (i / assignmentsPerLevel)) - 1;
+
+        // 50 pikselin lisäreunus ylös
+
+        return 50 + 2 * borderSize + level * (2 * borderSize + blockSize);
     }
 
     function removeOriginalCanvas() {
@@ -120,12 +135,26 @@ ProgressApp.controller('NewCourseController', function ($scope, $location, httpS
         return Math.floor((Math.random() * blockSize) + start);
     }
 
+    function directionOfCurve(directionAtTheMoment, i, assignmentsPerLevel) {
+        if (i % assignmentsPerLevel == 0) {
+            directionAtTheMoment = changeDirectionOfCurve(directionAtTheMoment);
+        }
+        return directionAtTheMoment;
+    }
+
+    function changeDirectionOfCurve(directionAtTheMoment) {
+        if (directionAtTheMoment === "left") {
+            return "right";
+        }
+        return "left";
+    }
+
     function getLocations() {
         var locations = []
 
         for (var i = 0; i < $scope.assignments.length; i++) {
             locations.push([$scope.assignments[i].location.x, $scope.assignments[i].location.y])
         }
-        return locations    
+        return locations
     }
 })
