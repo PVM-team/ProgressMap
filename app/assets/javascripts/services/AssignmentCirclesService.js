1ee30eb29a9470b5d261e3d2b719ea8d785c32b0
Pakarinen Pia P <pppakari@dat-12.cs.helsinki.fi>
2015-11-12 12:51:47 +0200

    percentages and assignments fetched from respective layers by assignment.bumber-1

--- app/assets/javascripts/services/AssignmentCirclesService.js
+++ app/assets/javascripts/services/AssignmentCirclesService.js
@@ -1,11 +1,11 @@
 ProgressApp.service('AssignmentCirclesService', function (MapScaleService) {
 
-    this.initializeCircle = function(assignment, students, assignmentLayer, percentageLayer, labelLayer) {
+    this.initializeCircle = function (assignment, students, assignmentLayer, percentageLayer, labelLayer) {
         var location = assignment.location;
         var percentageCompleted = assignment.doers.length / students.length * 100;
 
         var circle = new paper.Path.Circle(location, 35);
-        
+
         circle.fillColor = 'yellow';
         circle.fillColor.hue += percentageCompleted;
 
@@ -13,94 +13,84 @@ ProgressApp.service('AssignmentCirclesService', function (MapScaleService) {
 
         //assignment numbers over assignment circles
         var text = new paper.PointText({
-                        point: location,
-                        content: assignment.number,
-                        fillColor: 'white',
-                        fontSize: 20
+            point: location,
+            content: assignment.number,
+            fillColor: 'white',
+            fontSize: 20
         });
         labelLayer.addChild(text);
 
         //percentage over assignment circles
-        var percentageLocationPoint = {'x': location.x, 'y': location.y + 20 };
+        var percentageLocationPoint = {'x': location.x, 'y': location.y + 20};
 
         var percentage = new paper.PointText({
-                        point: percentageLocationPoint,
-                        content: Math.floor(percentageCompleted) + "%",
-                        fillColor: 'white'
+            point: percentageLocationPoint,
+            content: Math.floor(percentageCompleted) + "%",
+            fillColor: 'white'
         });
         percentageLayer.addChild(percentage);
         paper.view.update();
     }
 
-	/*this.drawCircle = function(assignment, students) {
-		var location = assignment.location;
-		var percentageCompleted = assignment.doers.length / students.length * 100;
+    /*this.drawCircle = function(assignment, students) {
+     var location = assignment.location;
+     var percentageCompleted = assignment.doers.length / students.length * 100;
 
-        var circle = new paper.Path.Circle(location, MapScaleService.getRelativeX(35));
-        circle.scale(window.innerWidth / MapScaleService.getPreviousWindowWidth());
-        
-        circle.fillColor = 'yellow';
-        circle.fillColor.hue += percentageCompleted;
+     var circle = new paper.Path.Circle(location, MapScaleService.getRelativeX(35));
+     circle.scale(window.innerWidth / MapScaleService.getPreviousWindowWidth());
 
-        //assignment numbers over assignment circles
-        var text = new paper.PointText({
-                   		point: location,
-                        content: assignment.number,
-                        fillColor: 'white',
-                        fontSize: MapScaleService.getRelativeX(20)
-        });
+     circle.fillColor = 'yellow';
+     circle.fillColor.hue += percentageCompleted;
 
-        //percentage over assignment circles
-        var percentageLocationPoint = {'x': location.x, 'y': location.y + MapScaleService.getRelativeX(20) };
+     //assignment numbers over assignment circles
+     var text = new paper.PointText({
+     point: location,
+     content: assignment.number,
+     fillColor: 'white',
+     fontSize: MapScaleService.getRelativeX(20)
+     });
 
-        var percentage = new paper.PointText({
-                        point: percentageLocationPoint,
-                        content: Math.floor(percentageCompleted) + "%",
-                        fillColor: 'white'
-        });
-        paper.view.update();
-	} */
+     //percentage over assignment circles
+     var percentageLocationPoint = {'x': location.x, 'y': location.y + MapScaleService.getRelativeX(20) };
 
-    this.updateCircleAfterNewDoer = function(assignment, students, assignmentLayer, percentageLayer) {
-        var location = assignment.location;
+     var percentage = new paper.PointText({
+     point: percentageLocationPoint,
+     content: Math.floor(percentageCompleted) + "%",
+     fillColor: 'white'
+     });
+     paper.view.update();
+     } */
 
-        var hitTest = assignmentLayer.hitTest(location);
+    this.updateCircleAfterNewDoer = function (assignment, students, assignmentLayer, percentageLayer) {
+        //var location = assignment.location;
 
-        if (! hitTest) {
+        //var hitTest = assignmentLayer.hitTest(location);
+
+        /*if (!hitTest) {
             console.log("circle not found. couldn't update.")
             return;
-        }
+        }*/
 
-        var circle = hitTest.item;
+        var circle = assignmentLayer.children[assignment.number-1];
         var percentageCompleted = assignment.doers.length / students.length * 100;
 
         circle.fillColor = 'yellow';
         circle.fillColor.hue += percentageCompleted;
 
-        var percentageLocationPoint = {'x': location.x, 'y': location.y + 20 };
+        //var percentageLocationPoint = {'x': location.x, 'y': location.y + 20};
 
         // while true ratkaisu ei toimi. jos ei löydy ekalla kerralla ko. positiosta mitään, niin ei löydy jatkossakaan
         // ongelma näyttäisi olevan spesifinen. ts. esim. mennessä tehtävään '9' Ohtuprojekti -kurssilla,
         // prosenttiluku päivittyy aina, mutta mennessä tehtävänää '3', ei koskaan.
 
-        console.log(location)
+        //hitTest = percentageLayer.hitTest(percentageLocationPoint);
 
-        hitTest = percentageLayer.hitTest(percentageLocationPoint);
+        //console.log(percentageLocationPoint);
+        //console.log("where it is: " + percentageLayer.children[assignment.number - 1].position);
+        //console.log("what is says: " + percentageLayer.children[assignment.number - 1].content);
 
-        if (! hitTest) {
-            // etsi child, jonka positio on x -ja y-suunnissa esim. alle 10px etäisyydellä assignmentin lokaatiosta (location).
-
-            for (var i = 0; i < percentageLayer.children.length; i++) {
-                var loc = percentageLayer.children[i].position;
-
-                console.log(loc);
-            }
-
-            console.log("percentage not found. couldn't update.")
-            return;
-        }
-
-        hitTest.item.content = Math.floor(percentageCompleted) + "%";
+        //hitTest.item.content = Math.floor(percentageCompleted) + "%";
+        percentageLayer.children[assignment.number - 1].content = Math.floor(percentageCompleted) + "%";
         paper.view.update();
     }
 })
