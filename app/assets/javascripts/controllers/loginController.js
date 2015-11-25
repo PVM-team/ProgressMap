0a82fb4b20f0dd1650b3deadc4919cfd638c1719
Teemu Ijäs <tijas@cs.helsinki.fi>
2015-11-25 15:38:40 +0200

    sign in and sign out buttons to /login page

--- app/assets/javascripts/controllers/loginController.js
+++ app/assets/javascripts/controllers/loginController.js
@@ -1,20 +1,24 @@
 ProgressApp.controller('LoginController', function($scope, $routeParams, $location, httpService) {
-    console.log('on login controller');
-
-    $scope.onSignIn = function(googleUser) {
+    window.onSignIn = onSignIn;
+    
+    function onSignIn(googleUser) {
         var profile = googleUser.getBasicProfile();
         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
         console.log('Email: ' + profile.getEmail());
+        console.log('Id_token: ' + googleUser.getAuthResponse().id_token)
     }
+
 
     $scope.signOut = function() {
         var auth2 = gapi.auth2.getAuthInstance();
+
+        console.log(auth2.currentUser.get().getBasicProfile());
         auth2.signOut().then(function () {
             console.log('User signed out.');
         });
-    }
+    };
 
 
 });
