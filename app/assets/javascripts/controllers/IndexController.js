b9a5a72601873cf7478c7b8d6041e6f4e91c4b6f
Teemu Ijäs <tijas@cs.helsinki.fi>
2015-12-03 18:09:16 +0200

    unused injection removed

--- app/assets/javascripts/controllers/IndexController.js
+++ app/assets/javascripts/controllers/IndexController.js
@@ -1,3 +1,3 @@
-ProgressApp.controller('IndexController', function($scope, $location, $compile, httpService, SessionService) {
+ProgressApp.controller('IndexController', function($scope, $location, $compile, httpService) {
     $scope.$parent.showNavigation = true;
 })
